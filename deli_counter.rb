dcbd703369bb3dd719e38f89c5d93dc0c9706f77
Chaim Bryski <luzybry94@gmail.com>
2020-08-05 22:13:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,10 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length == 0
-    puts ""
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}"
+    katz_deli.shift
 end
   
   
