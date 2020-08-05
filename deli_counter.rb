20831183815829cbe7edaf5e40425515cb63126b
Chaim Bryski <luzybry94@gmail.com>
2020-08-05 22:05:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,11 +12,8 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  if katz_deli.length == 0
-    puts "Welcome, #{name}. You are number 1 in line."
-  else
-    puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
-  end
+  katz_deli << name
+  
 end
   
   
