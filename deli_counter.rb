af76b79b2ca1ff1d699490025c23d0a71b3b36f5
Chaim Bryski <luzybry94@gmail.com>
2020-08-05 22:02:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      current_line << " #{index+1}. #{name}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length == 0
+    puts "Welcome, #{name}. You are number 1 in line."
+  else
+    puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
+  end
+end
+  
+  
